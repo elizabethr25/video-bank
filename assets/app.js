c2511dc9d71a3862d22fa15c75e6d538cce8ac0f
elizabethr25 <emrichardson820@gmail.com>
2018-07-18 11:32:58 -0700

    renamed tag to topic for clarity

--- assets/app.js
+++ assets/app.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
-  function onlyUnique(value, index, array) {
+  // Global function to find unique values in an array
+  let onlyUnique = (value, index, array) => {
     return array.indexOf(value) === index;
-  }
-
-  //Render tag buttons
-  let renderTagButtons = () => {
-    let tr = $("tr");
-    let tagsArr = [];
-
-    for (i = 1; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      tags = td.getAttribute("tag");
-      tagsArr.push(tags);
-    }
-
-    let uniqueTagsArr = tagsArr.filter(onlyUnique);
-    console.log("unique array:  " + uniqueTagsArr);
-
-    for (i = 0; i < uniqueTagsArr.length; i++) {
-      let b = $("<button>");
-      b.text(uniqueTagsArr[i]);
-      b.addClass("tagButton");
-      b.attr("button-value", uniqueTagsArr[i]);
-      $(".buttonDiv").append(b);
-    }
   };
-  renderTagButtons();
 
+  //Global function to show all table rows
   let renderFullTable = () => {
     $("#searchInput").val("");
     let tr = $("tr");
     tr.show();
   };
 
-  //Search button on click
+  //Render topic buttons function
+  let renderTopicButtons = () => {
+    let tr = $("tr");
+    let topicsArr = [];
+
+    for (i = 1; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      topics = td.getAttribute("topic");
+      topicsArr.push(topics);
+    }
+
+    let uniqueTopicsArr = topicsArr.filter(onlyUnique);
+    console.log("unique array:  " + uniqueTopicsArr);
+
+    for (i = 0; i < uniqueTopicsArr.length; i++) {
+      let b = $("<button>");
+      b.text(uniqueTopicsArr[i]);
+      b.addClass("topicButton");
+      b.attr("button-value", uniqueTopicsArr[i]);
+      $(".buttonDiv").append(b);
+    }
+  };
+
+  //   Calling renderTopixButtons function
+  renderTopicButtons();
+
+  //On click function for search button based on user input
   $(".searchBtn").on("click", function() {
     let input = $("#searchInput").val();
     let filter = input.toUpperCase();
@@ -53,8 +57,8 @@ $(document).ready(function() {
     }
   });
 
-  // Tag button on click
-  $(".tagButton").on("click", function() {
+  // On click function to query resources using premade buttons
+  $(".topicButton").on("click", function() {
     renderFullTable();
     let input = $(this).attr("button-value");
     let filter = input.toUpperCase();
@@ -62,8 +66,8 @@ $(document).ready(function() {
 
     for (i = 1; i < tr.length; i++) {
       let td = tr[i].getElementsByTagName("td")[0];
-      let tdTag = td.getAttribute("tag");
-      if (tdTag.toUpperCase() === filter) {
+      let tdTopic = td.getAttribute("topic");
+      if (tdTopic.toUpperCase() === filter) {
         tr[i].style.display = " ";
       } else {
         tr[i].style.display = "none";
